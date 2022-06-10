fec879f53389bc3bf718736b36215612d8ab3265
pedrocarvalho77 <pedromiguelrc99@gmail.com>
2022-06-11 00:57:48 +0100

    almost final version, only remains dealing with comments

--- src/lexer.pl
+++ src/lexer.pl
@@ -1,100 +1,12 @@
-%:- set_prolog_flag(double_quotes, chars).
+:- module(lexer, [tokenize/2]).
 
-lexem(int) --> [int].
-lexem(string) --> [string]. 
-lexem(+) --> [+].
-lexem(-) --> [-].
-lexem(*) --> [*].
-lexem(/) --> [/].
-%lexem(%) --> [%].
-lexem(var) --> [var].
-lexem(:=) --> [:=].
-lexem(=) --> [=].
-lexem(<>) --> [<>].
-lexem(<) --> [<].
-lexem(<=) --> [<=].
-lexem(>) --> [>].
-lexem(>=) --> [>=].
-lexem(if) --> [if].
-lexem(then) --> [then].
-lexem(else) --> [else].
-%lexem(tok_left) --> [(].
-lexem(")") --> ")".
-lexem(;) --> [;].
-lexem(while) --> [while].
-lexem(do) --> [do].
-lexem(function) --> [function].
-lexem(:) --> [:].
-lexem("let") --> ["let"].
-lexem(in) --> [in].
-lexem(end) --> [end].
-lexem(scani) --> [scani].
-lexem(printi) --> [printi].
-lexem(&) --> [&].
-%lexem(|) --> [|].
-lexem(for) --> [for].
-lexem(to) --> [to].
-lexem(break) --> [break].
-lexem(print) --> [print].
-%lexem([) --> [[].
-%lexem(]) --> []].
-lexem(of) --> [of].
-lexem(tok_comma) --> [,].
-
-lexem(int(NA)) -->  
-    number(A),
-    !,                     % longest input match
-    {number_chars(NA,A)}.
-
-lexem(id(IA)) -->
-    identifier(I),
-    !,                     % longest input match
-    {atom_chars(IA,I)}.
-    
-whitespace -->
-    [W],
-    {char_type(W,space)}. % space is whitespace
-
-identifier([L|Ls]) -->
-    first_letter(L),
-    ident(Ls).
-    
-ident([L|Ls]) -->
-    first_letter(L),
-    ident(Ls).
-ident([]) -->
-        [].
-    
-first_letter(L) -->
-    uppercase(L).
-first_letter(L) -->
-    lowercase(L).
-%first_letter("_") -->
-%    [_]. 
-    
-uppercase(L) -->
-    [L],                   % uppercase letters
-    {char_type(L,upper)}.  
-     
-lowercase(L) -->
-    [L],                   % lowercase letters
-    {char_type(L,lower)}.  
-    
-number([D|Ds]) -->        % numbers are
-    digit(D),              % a digit followed
-    digits(Ds).            % by other digits
-    
-digits([D|Ds]) -->
-    digit(D),
-    digits(Ds).
-digits([]) -->
-    [].
-    
-digit(D) -->              % a single digit
-    [D],
-    {char_type(D,digit)}.
-    
 % entry point!!!!!!!!!!!!!
+tokenize(Input, Result) :-
+    %phrase(lexer(Result), Input),
+    phrase_from_file(lexer(Result), Input),
+    %write(Result),
+    !.
+
 lexer(Input) -->             
     whitespace,           % whitespace is ignored
     lexer(Input).
@@ -104,7 +16,74 @@ lexer([Token | Tokens]) -->
 lexer([]) -->
     [].
 
-tokenize(Input) :-
-    phrase(lexer(L), Input),
-    write(L),
-    !.
+whitespace -->
+    [W],
+    {char_type(W,space)}. % space is whitespace
+
+anything([]) --> [].
+anything([A|T]) --> [A], anything(T).
+
+lexem([]) --> "/*", anything(A), "*/".
+
+lexem(tok_type_int) --> "int".
+lexem(tok_type_string) --> "string". 
+lexem(tok_plus) --> "+".
+lexem(tok_minus) --> "-".
+lexem(tok_mult) --> "*".
+lexem(tok_div) --> "/".
+lexem(tok_mod) --> "%".
+lexem(tok_var) --> "var".
+lexem(tok_assign) --> ":=".
+lexem(tok_equal) --> "=".
+lexem(tok_diff) --> "<>".
+lexem(tok_less) --> "<".
+lexem(tok_less_or_equal) --> "<=".
+lexem(tok_greater) --> ">".
+lexem(tok_greater_or_equal) --> ">=".
+lexem(tok_if) --> "if".
+lexem(tok_then) --> "then".
+lexem(tok_else) --> "else".
+lexem(tok_lparen) --> "(".
+lexem(tok_rparen) --> ")".
+lexem(tok_semicolon) --> ";".
+lexem(tok_while) --> "while".
+lexem(tok_do) --> "do".
+lexem(tok_func) --> "function".
+lexem(tok_colon) --> ":".
+lexem(tok_let) --> "let".
+lexem(tok_in) --> "in".
+lexem(tok_end) --> "end".
+lexem(tok_scani) --> "scani".
+lexem(tok_printi) --> "printi".
+lexem(tok_and) --> "&".
+lexem(tok_or) --> "|".
+lexem(tok_for) --> "for".
+lexem(tok_to) --> "to".
+lexem(tok_break) --> "break".
+lexem(tok_print) --> "print".
+lexem(tok_l_square_bracket) --> "[".
+lexem(tok_r_square_bracket) --> "]".
+lexem(tok_of) --> "of".
+lexem(tok_comma) --> ",".
+
+lexem(tok_num(NA)) --> number(A), !, {number_chars(NA,A)}.
+
+lexem(tok_id(IA)) --> identifier(I), !, {atom_chars(IA,I)}.
+    
+identifier([L|Ls]) --> first_symbol(L), ident(Ls).
+    
+ident([L|Ls]) --> symbol(L), ident(Ls).
+ident([]) --> [].
+    
+%csymf : Char is a letter (upper- or lowercase) or the underscore (_). These are valid first characters for C and Prolog symbols.
+first_symbol(L) --> [L], {char_type(L,csymf)}. 
+
+% csym : Char is a letter (upper- or lowercase), digit or the underscore (_). These are valid C and Prolog symbol characters.
+symbol(L) --> [L], {char_type(L,csym)}.
+
+number([D|Ds]) --> digit(D), digits(Ds).           
+    
+digits([D|Ds]) --> digit(D), digits(Ds).
+digits([]) --> [].
+    
+digit(D) --> [D],{char_type(D,digit)}.
